7f595adf07268b4ce61b8229d3d6d21cee051a47
Dhanushtn03 <dhanush03nadtm@gmail.com>
2024-10-21 20:21:15 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "project-amazon-ds3-bucket"  
+    bucket = "project-amazon-ds3-bucket-app"  
     key    = "EKS/terraform.tfstate"  
     region = "us-east-1" 
   }
